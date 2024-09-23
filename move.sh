cba51814cc3fb56efbcb0a97fdbc2a0472f9d7ac
mmg-moovingon <98336557+mmg-moovingon@users.noreply.github.com>
2024-09-23 12:01:15 +0300

    Delete move.sh

--- move.sh
+++
@@ -1,4 +0,0 @@
-for server in moovingon@logic{2,3,7..13,17..31}.rtbsrv.com; do
-    echo "Copying to $server..."
-    scp -i ~/.ssh/mmg-keys/moovingon.pem -r ~/prometheus_custom_metrics "$server:/tmp" || echo "Failed to copy to $server"
-done
